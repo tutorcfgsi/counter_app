9761e37a21c7ee494578a1ab0fa915adce6ed362
Raul Reyes <tutorcfgsi@hotmail.com>
2022-09-27 11:42:06 +0200

    first commit

---
+++ counter_app/lib/screens/home_sreen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('HomeScreen'),
+        elevation: 10.0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[Text('Número de Pulsaciones'), Text('0')],
+        ),
+      ),
+    );
+  }
+}
